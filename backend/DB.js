e9e2870dc5d6239d54b8c38cd800327e84250a17
Aksh4code <akkisingh8266@gmail.com>
2024-09-18 23:40:19 +0530

    removed credentials

--- backend/DB.js
+++ backend/DB.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://akkisingh8266:7Ks1540mOobvY0le@cluster0.y6ozh.mongodb.net/todoApp");
+mongoose.connect("mongodb+srv://<username>:<password>@cluster0.y6ozh.mongodb.net/todoApp");
 
 const todoSchema = mongoose.Schema({
     title :String,
